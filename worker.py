6c7f1cb72eb1ffed7f224b078efff847dd288926
Sergey <46491304+mrflusha@users.noreply.github.com>
2022-06-08 14:24:11 +0300

    Create worker.py

---
+++ worker.py
@@ -0,0 +1,9 @@
+class tg_req:
+	def __init__ (self, username, user_id, date_sended):
+		self.username = username
+		self.user_id = user_id
+		self.date = date_sended
+		
+		
+test = tg_req("cry1337","2","23.04.2022")
+
