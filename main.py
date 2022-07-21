f9f1061a5747d5512b2ef8f1877a769ae57668f5
mrf <marv1n.minky@gmail.com>
2022-07-21 12:30:21 +0300

    add dicret output

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from datetime import datetime, timedelta
 
 
 t = timedelta(days = 280)
+h = timedelta(days = 203)
 date_dict ={
 'day': int(input('enter date: ')),
 'month': int(input('enter month: ')),
@@ -15,6 +16,12 @@ date_dict ={
 date = datetime(day= date_dict['day'], month = date_dict['month'], year= date_dict['year'])
 
 
-born_day = date + t
 
-print(born_day.strftime("%d/%m/%Y"))
+
+
+born_day = date + t
+hospitality_date = date + h 
+print("date of born\t" + born_day.strftime("%d/%m/%Y") + "\ndate of dicret\t" + hospitality_date.strftime("%d/%m/%Y"))
+
+
+
